f5dc4e5bbde8b6d54effd65eee9ae2e034ba0fca
AhmedOT <iachmed@me.com>
2019-07-22 14:44:10 -0700

    Change Color Button is Working on iPX

--- Hello Color/ViewController.swift
+++ Hello Color/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isPurple = false
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ChangeColor(_ sender: Any) {
+        if isPurple {
+            view.backgroundColor = UIColor.red
+            isPurple = false
+        } else {
+            view.backgroundColor = UIColor.purple
+            isPurple = true
+        }
+    }
+    
 }
 
